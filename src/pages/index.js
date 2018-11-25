04f77a1075324016ebec9e70385606e406cf6bb7
hesam <hesamkaveh@github.com>
2018-11-25 08:26:17 +0330

    title of home changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React, {Component} from "react"
 import { graphql} from "gatsby"
 import Layout from "../components/layout"
 import PostPrev from "../components/PostPrev"
+import Helmet from "react-helmet";
 
 
 class Home extends Component {
@@ -10,6 +11,9 @@ class Home extends Component {
 
         return (
             <Layout>
+                <Helmet>
+                    <title>{data.site.siteMetadata.title} | روزنوشته های یک توسعه دهنده</title>
+                </Helmet>
 
                 {data.allWordpressPost.edges.map(({node}) => (
                     <PostPrev node={node} key={node.slug} />
@@ -25,6 +29,12 @@ export default Home
 // Set here the ID of the home page.
 export const pageQuery = graphql`
   query {
+  site {
+      siteMetadata {
+        title
+        subtitle
+      }
+    }
     allWordpressPost(sort: { fields: [date], order: DESC }) {
       edges {
         node {
