3e7db0c0013c2c720dfe17fc8dcebabccda3decf
hesam <hesamkaveh@github.com>
2018-12-28 20:18:32 +0330

    remove console.log

--- src/templates/post.js
+++ src/templates/post.js
@@ -71,9 +71,6 @@ class PostTemplate extends Component {
                     <div className="postContainer">
                         <Title dangerouslySetInnerHTML={{__html: post.title}}/>
                         <PostIcons node={post}/>
-                        {console.log(post.featured_media.localFile.childImageSharp.original.src)}
-                        {console.log(post.featured_media.localFile.childImageSharp.original.src)}
-                        {console.log(post.featured_media.localFile.childImageSharp.original.src)}
                         {post.featured_media ? <img alt='' className="FeaturedPostImg"
                                                     src={ post.featured_media.localFile.childImageSharp.original.src }/> : null}
                         <div id='content' dangerouslySetInnerHTML={{__html: (post.content.replace(/http:\/\/backend\.hesamkaveh\.com\/wp-content\/uploads/g,'https://backend.hesamkaveh.com/wp-content/uploads'))}}/>
