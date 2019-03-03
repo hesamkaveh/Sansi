657828d0e2e6217a4af46f826e197e473a4ddb4a
hesam <hesamkaveh@github.com>
2019-03-03 16:59:15 +0330

    description of rss updated

--- hesamkaveh_builder.sh
+++
@@ -1,7 +0,0 @@
-cd /projects/hesamkaveh_ui &&
-git pull &&
-rm -Rf .cache public node_modules &&
-yarn &&
-gatsby build &&
-sudo rm -Rf /home/admin/web/hesamkaveh.com/public_html/* &&
-mv /projects/hesamkaveh_ui/public/* /home/admin/web/hesamkaveh.com/public_html/
